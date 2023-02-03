fb2d251f93cb0a9a09055ebc05ec8ad7c8d0f113
Fatih <kilic8118@gmail.com>
2023-02-03 14:40:42 +0300

    Promises lucky draw

--- example.mjs
+++
@@ -1,6 +0,0 @@
-import * as fs from "node:fs";
-
-fs.writeFile("example.txt", "Hello World! Sorry for being late.", (err) => {
-    if (err) throw err;
-    console.log("The file has been saved!");
-    });
