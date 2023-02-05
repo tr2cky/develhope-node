219bca8d6d56a36bd67497444dc3c86bf3f69e46
Fatih <kilic8118@gmail.com>
2023-02-05 15:52:45 +0300

    Exercise 1, Set up a simple Express App

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,52 @@
-const TODO: string = "start writing your Express API server here :)";
+import express from "express";
+import "express-async-errors";
+import morgan from "morgan";
 
-console.log(TODO);
+
+const app = express();
+const port = 3000;
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+type Planet = {
+  id: number;
+  name: string;
+};
+
+type Planets = Planet[];
+
+let planets: Planets = [
+  {
+    id: 1,
+    name: "Earth",
+  },
+  {
+    id: 2,
+    name: "Mars",
+  },
+];
+
+app.get("/api/planets", (req, res) => {
+  res.status(200).json(planets);
+});
+app.get("/api/planets/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const planet = planets.find((planet) => planet.id === id);
+  res.status(200).json(planet);
+});
+
+app.post("/api/planets", (req, res) => {
+    const {id, name} = req.body;
+    const newPlanet = {id, name};
+
+    planets = [...planets, newPlanet];
+    
+    console.log(planets);
+    
+    res.status(201).json({message: "Planet added successfully"});
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
